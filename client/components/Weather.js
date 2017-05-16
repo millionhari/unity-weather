22e18f41a60c3a708fbf91d48722821540ee444e
Michael Hari <millionhari@Michaels-MacBook-Pro.local>
2017-05-16 02:26:06 -0700

    feat(): Update styling

--- client/components/Weather.js
+++ client/components/Weather.js
@@ -10,8 +10,22 @@ import { each, map } from 'lodash';
 export default class Weather extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.getFromLocalStorage('unity-weather');
+    this.state = this.getFromLocalStorage('unity-weather') || {};
     this.setWeatherState = this.setWeatherState.bind(this);
+  }
+
+  componentWillMount() {
+    const data = this.getFromLocalStorage('unity-weather');
+
+    _.each(data, (city) => {
+      getWeather((val)=> {
+        const newObj = this.addToObj(data, {
+          [val.name]: val
+        });
+        this.setToLocalStorage('unity-weather', newObj);
+        this.setState(newObj);
+      }, this.props.apiKey, city.name);
+    })
   }
 
   addToObj(obj, addObj) {
